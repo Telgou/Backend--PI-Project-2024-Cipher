f647d7c38bf561f26773a18860e040424665ab76
Telgou <gamgamiahmed@yahoo.fr>
2024-02-28 06:03:32 +0100

    pre register+sending email to complete registration+complete registration

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,36 @@
 import bcrypt from "bcrypt";
+import crypto from 'crypto';
+import nodemailer from "nodemailer";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
+import PreUser from "../models/preUser.js";
+
+/* Pregister USER */ 
+export const pregister = async (req, res) => {
+  try {
+    const {
+      email,
+    } = req.body;
+    console.log(req.body)
+
+    console.log(email)
+    const newPreUser = new PreUser({ email });
+
+    const savedPreUser = await newPreUser.save();
+    const token = generateRandomToken(10);
+    savedPreUser.token=token;
+    await savedPreUser.save();
+
+    // Send an email with the token to the user
+    await sendTokenEmail(email, token);
+
+
+    res.status(201).json("pregistered correctly");
+  } catch (err) {
+    console.error("preRegister error:", err);
+    res.status(500).json({ error: err.message });
+  }
+};
 
 /* REGISTER USER */
 export const register = async (req, res) => {
@@ -8,14 +38,19 @@ export const register = async (req, res) => {
     const {
       firstName,
       lastName,
-      email,
       password,
       picturePath,
       friends,
       location,
       occupation,
+      tok,
     } = req.body;
     //console.log('Request Body:', req.body);
+
+     const preUser = await PreUser.findOne({ token: tok });
+     if (!preUser) {
+       return res.status(401).json({ error: "PreUser not found for the provided token" });
+     }
 
     const salt = await bcrypt.genSalt();
     const passwordHash = await bcrypt.hash(password, salt);
@@ -23,14 +58,14 @@ export const register = async (req, res) => {
     const newUser = new User({
       firstName,
       lastName,
-      email,
+      email : preUser.email,
       password: passwordHash,
       picturePath,
       friends,
       location,
       occupation,
-      viewedProfile: Math.floor(Math.random() * 10000),
-      impressions: Math.floor(Math.random() * 10000),
+      viewedProfile: 0,
+      impressions: 0,
     });
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
@@ -58,4 +93,30 @@ export const login = async (req, res) => {
     //console.error("Login error:", err);
     res.status(500).json({ error: err.message });
   }
+};
+
+
+const sendTokenEmail = async (email, token) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "gamgamitelgou@gmail.com",
+      pass: "tmpn cjzq eyrk epjl",
+    },
+  });
+
+  const mailOptions = {
+    from: "gamgamitelgou@gmail.com",
+    to: email,
+    subject: "Registration Token",
+    text: `Kindly continue to : http://localhost:3000/tok=${token} and complete the registration`,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
+
+const generateRandomToken = (length) => {
+  return crypto.randomBytes(Math.ceil(length / 2))
+    .toString('hex')
+    .slice(0, length);
 };
---
+++ models/preUser.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const PreUserSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+      validate: {
+        validator: function (value) {
+          return value.endsWith("@esprit.tn");
+        },
+        message: props => `${props.value} is not a valid email, it must end with "@esprit.tn"`,
+      },
+    },
+    token: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const PreUser = mongoose.model("PreUser", PreUserSchema);
+export default PreUser;
